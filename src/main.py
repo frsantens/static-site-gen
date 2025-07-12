c4aca1b39ae54896f98e873688f284c72d77a602
frsantens <frsantens@gmail.com>
2025-07-12 21:44:23 +0000

    copy from static to public when running main.sh

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
+from static_to_public import copy_from_to
+import sys
 
-def main():
-    return None
+def main(argv=None):
+    # if len(argv) > 1:
+    #     src = argv[1]
+    #     dst = argv[2]
+    # verbose = False
+    # if len(sys.argv) > 3 and sys.argv[3] == "-v":
+    #     verbose = True
+    #     copy_from_to(src='./static', dst='./public', verbose=False)
+    # else:
+    src = './static'
+    dst = './public'
+    verbose = False
+    copy_from_to(src, dst, verbose)
 
 
-
-main()
+if __name__ == "__main__":
+    main()  # the argv[0] is the current filename.
---
+++ src/static_to_public.py
@@ -0,0 +1,44 @@
+from genericpath import isfile
+import os
+import shutil
+from tabnanny import verbose
+
+def copy_from_to(dir_src, dir_dest, verbose):
+    if not os.path.isdir(dir_src):
+        raise Exception('not a directory, takes two dirs as arguments')
+    if not os.path.exists(dir_src):
+        raise Exception('source directory does not exist')
+    abs_src = os.path.abspath(dir_src)
+    abs_dest = os.path.abspath(dir_dest)
+    if os.path.exists(abs_dest):
+        shutil.rmtree(abs_dest)
+        if verbose:
+            print("STEP 1 removing", dir_dest, 'to remake it at', abs_dest, '\n')
+    os.mkdir(abs_dest)
+    
+    copy_recursive(abs_src, abs_dest, verbose)
+    
+def copy_recursive(src, dest, verbose):
+    entries_list = os.listdir(src)
+    if verbose:
+        print("source dir contains:", entries_list, '\n')
+    dest_nested = dest
+    for entr in entries_list:
+        entr_path = os.path.join(src, entr)
+        if os.path.isdir(entr_path):            
+            dest_nested = os.path.join(dest, entr)
+            if verbose:
+                print(" filepath:", os.path.abspath(entr_path))
+                print(f'  ->  making dir {entr} at dest={dest_nested}')
+                print(f"\ndoing recursive: copy_recursive({entr}, {dest_nested})")
+            os.mkdir(dest_nested)
+            copy_recursive(entr_path, dest_nested, verbose)
+        elif os.path.isfile(entr_path):
+            if verbose:
+                print(" filepath:", os.path.abspath(entr_path))
+                print(f"   -> coppying file {entr} to {dest}\n")
+            shutil.copy(entr_path,dest)
+            
+            
+
+    
